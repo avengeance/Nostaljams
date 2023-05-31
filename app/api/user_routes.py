02a3e197cca8b8b6a3f9e220d53d3d3a22c9b7c6
Gek <gakidogeck@gmail.com>
2023-05-30 21:56:20 -0700

    progress on first two kanban

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, SongImage, Comment, SongLike
 
 user_routes = Blueprint('users', __name__)
 
@@ -26,8 +26,31 @@ def user(id):
 
 #view all user songs
 @user_routes.route('/<int:id>/songs', method=['GET'])
-def fxn():
-    pass
+def get_user_songs(id):
+    # we need to match the id from the params to our user id
+    user = User.query.get(id)
+    if (user):
+        songs_list = []
+        for song in user.songs:
+            song_dict = song.to_dict()
+            song_dict["SongImage"] = {
+                "id": song.song_images[0].id if song.song_images else None,
+                "img_url": song.song_images[0].img_url if song.song_images else None
+            }
+            song_dict["SongCommentsCnt"] = song.comments.count()
+            song_dict["SongLikesCnt"] = song.song_likes.count()
+            songs_list.append(song_dict)
+
+        res = {
+            "UserSongs": songs_list
+        }
+        return jsonify(res), 200
+    else:
+        res = {
+            "message": "User couldn't be found",
+            "statusCode": 404
+        }
+        return jsonify(res), 404
 
 #view all playlists by user
 @user_routes.route('/<int:id>/playlists', method=['GET'])
