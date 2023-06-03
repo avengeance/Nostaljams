3c7b042f36150f44fe6cedb0098536c29cf554ff
Gek <gakidogeck@gmail.com>
2023-06-03 12:51:57 -0700

    modified alembic, playlist and user models, and playlist in user routes

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,33 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
-class Playlist(db.Model):
-    __tablename__='playlists'
-
-    if environment == 'production':
-        __table_args__ = {'schema': SCHEMA}
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.now)
-    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
-
-    users = db.relationship('User', back_populates='playlists')
-    playlist_likes = db.relationship('PlaylistLike', back_populates='playlists')
-    playlist_songs = db.relationship('PlaylistSong', back_populates='playlists')
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'userId': self.user_id,
-            'name': self.name,
-            'description': self.description,
-            'createdAt': self.created_at,
-            'updatedAt': self.updated_at
-        }
-
 class PlaylistSong(db.Model):
     __tablename__='playlist_songs'
 
@@ -35,8 +8,8 @@ class PlaylistSong(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')), nullable=False)
-    playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
+    playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
 
     songs = db.relationship('Song', back_populates='playlist_songs')
     playlists = db.relationship('Playlist', back_populates='playlist_songs')
@@ -46,4 +19,33 @@ class PlaylistSong(db.Model):
             'id': self.id,
             'songId': self.song_id,
             'playlistId': self.playlist_id
+        }
+
+class Playlist(db.Model):
+    __tablename__='playlists'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    users = db.relationship('User', back_populates='playlists')
+    # playlist_likes = db.relationship('PlaylistLike', back_populates='playlists')
+    # playlist_songs = db.relationship('PlaylistSong', back_populates='playlists')
+    songs = db.relationship('Song', secondary=PlaylistSong.__table__)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.user_id,
+            'name': self.name,
+            'description': self.description,
+            'songs': [song.to_dict() for song in self.songs],
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
         }
