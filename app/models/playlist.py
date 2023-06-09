bfd5e2e87414cc3f09694f6b83e8763a882b679c
Calvin Le <email.calvinle@gmail.com>
2023-06-08 21:20:09 -0700

    message

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -18,7 +18,7 @@ class Playlist(db.Model):
     users = db.relationship('User', back_populates='playlists')
     likes = db.relationship('PlaylistLike', back_populates='playlist')
     playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
-    songs = db.relationship('Song', back_populates='playlists', secondary='playlist_songs', overlaps="playlist_songs,songs", cascade='all, delete')
+    songs = db.relationship('Song', back_populates='playlists', secondary=Playlist_Song.__table__, overlaps="playlist_songs,songs", cascade='all, delete')
 
     def to_dict(self):
         return {
