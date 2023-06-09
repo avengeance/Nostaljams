b5d36db19435268e6fac21f8e6caf8bd70b2ae29
Calvin Le <email.calvinle@gmail.com>
2023-06-08 22:38:20 -0700

    the final

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -17,7 +17,7 @@ class Playlist(db.Model):
 
     users = db.relationship('User', back_populates='playlists')
 
-    playlist_likes = db.relationship('PlaylistLike', back_populates='playlist_likes')
+    playlist_likes = db.relationship('PlaylistLike', back_populates='playlist')
     playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
 
     songs = db.relationship('Song', back_populates='playlists', secondary='playlist_songs', overlaps="playlist_songs", cascade='all, delete')
