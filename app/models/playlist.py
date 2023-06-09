962049aa539939381368c0de50f59a1267906841
Calvin Le <email.calvinle@gmail.com>
2023-06-08 21:20:31 -0700

    will work

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,6 +1,26 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
+
+class PlaylistSong(db.Model):
+    __tablename__ = 'playlist_songs'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
+    playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
+
+    songs = db.relationship('Song', back_populates='playlist_songs', overlaps="playlists")
+    playlist = db.relationship('Playlist', back_populates='playlist_songs', overlaps="playlists")
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'songId': self.song_id,
+            'playlistId': self.playlist_id
+        }
+
 class Playlist(db.Model):
     
     __tablename__='playlists'
@@ -31,22 +51,3 @@ class Playlist(db.Model):
             'updatedAt': self.updated_at
         }
 
-
-class PlaylistSong(db.Model):
-    __tablename__ = 'playlist_songs'
-
-    if environment == 'production':
-        __table_args__ = {'schema': SCHEMA}
-
-    id = db.Column(db.Integer, primary_key=True)
-    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
-    playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
-
-    songs = db.relationship('Song', back_populates='playlist_songs', overlaps="playlists")
-    playlist = db.relationship('Playlist', back_populates='playlist_songs', overlaps="playlists")
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'songId': self.song_id,
-            'playlistId': self.playlist_id
-        }
