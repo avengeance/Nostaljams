78835e123bb57668f62d4aed622e85ccbbb35e2e
Calvin Le <email.calvinle@gmail.com>
2023-06-08 21:29:33 -0700

    push

---
+++ app/models/playlist_song.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class PlaylistSong(db.Model):
+    __tablename__ = 'playlist_songs'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
+    playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
+
+    songs = db.relationship('Song', back_populates='playlist_songs', overlaps="playlists")
+    playlist = db.relationship('Playlist', back_populates='playlist_songs', overlaps="playlists")
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'songId': self.song_id,
+            'playlistId': self.playlist_id
+        }
