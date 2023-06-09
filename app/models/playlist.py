40d828eb7158c1788f657b319b9b4dd874bc64a4
Calvin Le <email.calvinle@gmail.com>
2023-06-08 20:18:28 -0700

    moved import

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 from .images import SongImage
-from .song import Song
+
 
 class PlaylistSong(db.Model):
     __tablename__ = 'playlist_songs'
@@ -52,22 +52,4 @@ class Playlist(db.Model):
         }
 
 
-# class PlaylistSong(db.Model):
-#     __tablename__='playlist_songs'
-
-#     if environment == 'production':
-#         __table_args__ = {'schema': SCHEMA}
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
-#     playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
-
-#     songs = db.relationship('Song', back_populates='playlist_songs')
-#     playlists = db.relationship('Playlist', back_populates='playlist_songs')
-
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'songId': self.song_id,
-#             'playlistId': self.playlist_id
-#         }
+from .song import Song
