381aaddc955b4a2d0d975f59fcdd34cf62486a35
Calvin Le <email.calvinle@gmail.com>
2023-06-08 21:24:35 -0700

    lets do it

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -38,7 +38,7 @@ class Playlist(db.Model):
     users = db.relationship('User', back_populates='playlists')
     likes = db.relationship('PlaylistLike', back_populates='playlist')
     playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
-    songs = db.relationship('Song', back_populates='playlists', secondary=Playlist_Song.__table__, overlaps="playlist_songs,songs", cascade='all, delete')
+    songs = db.relationship('Song', back_populates='playlists', secondary=PlaylistSong.__table__, overlaps="playlist_songs,songs", cascade='all, delete')
 
     def to_dict(self):
         return {
@@ -50,4 +50,3 @@ class Playlist(db.Model):
             'createdAt': self.created_at,
             'updatedAt': self.updated_at
         }
-
