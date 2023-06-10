7da2d66f6c3dadc94f53e9cdca707b6ac9e5055b
Calvin Le <email.calvinle@gmail.com>
2023-06-10 14:06:41 -0700

    pesky s

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,7 +12,7 @@ import * as likeActions from "./store/likes";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import Navigation from "./components/Navigation";
-import Song from "./components/Songs";
+import Song from "./components/Song";
 import CreateSong from "./components/CreateSong";
 
 function App() {
