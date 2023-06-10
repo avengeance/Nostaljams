23dc849cc52b1572c9a5dd5b148a8c21856d14e7
Calvin Le <email.calvinle@gmail.com>
2023-06-10 12:02:31 -0700

    more components

---
+++ react-app/src/components/EditPlaylist/index.js
@@ -0,0 +1,48 @@
+import { useEffect,useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import * as PlaylistActions from "../../store/playlists";
+import "./EditPlaylist.css";
+
+const EditPlaylist = () => {
+    const dispatch = useDispatch();
+    const { playlistId } = useParams();
+    const history = useHistory();
+
+    const playlists = useSelector((state) => (state.playlists.playlists.Playlists));
+    const playlist = playlists.find((playlist) => playlist.id === playlistId);
+    const user = useSelector((state) => state.session.user);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const currentPlaylist = useSelector((state) => state.playlists.currentPlaylist);
+
+    useEffect(() => {
+        dispatch(PlaylistActions.getPlaylistThunk(playlistId));
+    }, [dispatch, playlistId]);
+
+    useEffect(() => {
+        if (playlist) {
+            setName(playlist.name);
+            setDescription(playlist.description);
+        }
+    }, [playlist]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const payload = {
+            name,
+            description,
+        };
+        const data = await dispatch(PlaylistActions.editPlaylistThunk(payload, playlistId));
+        if (data){
+            history.push('/playlists/current');
+        }
+    }
+
+    return(
+        <></>
+    )
+}
