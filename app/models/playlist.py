335c2ba26deb5428a1f45f96989e4d09e5d53108
Calvin Le <email.calvinle@gmail.com>
2023-06-09 19:34:32 -0700

    here we go

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -1,9 +1,43 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
-class Playlist(db.Model):
+# class Playlist(db.Model):
     
-    __tablename__='playlists'
+#     __tablename__='playlists'
+
+#     if environment == 'production':
+#         __table_args__ = {'schema': SCHEMA}
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+#     name = db.Column(db.String(255), nullable=False)
+#     description = db.Column(db.String(255))
+#     created_at = db.Column(db.DateTime, default=datetime.now)
+#     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+#     users = db.relationship('User', back_populates='playlists')
+
+#     playlist_likes = db.relationship('PlaylistLike', back_populates='playlist')
+#     playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
+
+#     songs = db.relationship('Song', back_populates='playlists', secondary='playlist_songs', overlaps="playlist_songs", cascade='all, delete')
+
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'userId': self.user_id,
+#             'name': self.name,
+#             'description': self.description,
+#             'songs': [song.to_dict() for song in self.songs],
+#             'createdAt': self.created_at,
+#             'updatedAt': self.updated_at
+#         }
+
+
+# Playlist.playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
+
+class Playlist(db.Model):
+    __tablename__ = 'playlists'
 
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
@@ -16,11 +50,11 @@ class Playlist(db.Model):
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
     users = db.relationship('User', back_populates='playlists')
-
     playlist_likes = db.relationship('PlaylistLike', back_populates='playlist')
-    playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
 
     songs = db.relationship('Song', back_populates='playlists', secondary='playlist_songs', overlaps="playlist_songs", cascade='all, delete')
+
+    playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
 
     def to_dict(self):
         return {
@@ -32,6 +66,3 @@ class Playlist(db.Model):
             'createdAt': self.created_at,
             'updatedAt': self.updated_at
         }
-
-
-Playlist.playlist_songs = db.relationship('PlaylistSong', back_populates='playlist', cascade='all, delete')
--- app/models/playlist_song.py
+++ app/models/playlist_song.py
@@ -2,6 +2,26 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
 
+# class PlaylistSong(db.Model):
+#     __tablename__ = 'playlist_songs'
+
+#     if environment == 'production':
+#         __table_args__ = {'schema': SCHEMA}
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
+#     playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
+
+#     # songs = db.relationship('Song', back_populates='playlist_songs', overlaps="playlists")
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'songId': self.song_id,
+#             'playlistId': self.playlist_id
+#         }
+
+# PlaylistSong.playlist = db.relationship('Playlist', back_populates='playlist_songs', overlaps="playlist_songs")
+
 class PlaylistSong(db.Model):
     __tablename__ = 'playlist_songs'
 
@@ -12,12 +32,11 @@ class PlaylistSong(db.Model):
     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')))
     playlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')))
 
-    # songs = db.relationship('Song', back_populates='playlist_songs', overlaps="playlists")
+    playlist = db.relationship('Playlist', back_populates='playlist_songs', lazy='subquery')
+
     def to_dict(self):
         return {
             'id': self.id,
             'songId': self.song_id,
             'playlistId': self.playlist_id
         }
-
-PlaylistSong.playlist = db.relationship('Playlist', back_populates='playlist_songs', overlaps="playlist_songs")
