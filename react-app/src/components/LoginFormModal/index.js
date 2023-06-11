3b3c8e23d4a16850c051d2370b4d4ddf03e91c85
Calvin Le <email.calvinle@gmail.com>
2023-06-11 15:22:27 -0700

    fixed login form modal

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -11,26 +11,25 @@ function LoginFormModal() {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
-  const [crendential, setCredential] = useState("");
 
   const MIN_USERNAME_LENGTH = 4;
   const MIN_PASSWORD_LENGTH = 6;
 
-  const validCredential = crendential.length >= MIN_USERNAME_LENGTH;
+  const validCredential = email.length >= MIN_USERNAME_LENGTH;
   const validPassword = password.length >= MIN_PASSWORD_LENGTH;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrors([]);
-    const data = await dispatch(login(email, password));
-    if (data && data.errors){
-      setErrors(data.errors);
-    }
-    else {
-      setErrors(["The provided credentials are invalid"]);
-    }
-    closeModal()
-
+    const data = await dispatch(sessionActions.login(email, password));
+      if (data && data.errors ) {
+        setErrors(data.errors);
+      } else {
+        setErrors(["The provided credentials are invalid"]);
+      }
+      if(data.ok){
+        closeModal()
+      }
   };
 
   const handleDemoLogin = async (e) => {
@@ -72,7 +71,7 @@ function LoginFormModal() {
         <div id='div-login-submit'>
           <button type="submit"
             id='login-button'
-          disabled={!validCredential || !validPassword}
+            disabled={!validCredential || !validPassword}
           >Log In</button>
         </div>
         <div className="demo-login">
