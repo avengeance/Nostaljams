0a87358981262ca71488644bec7284f629b97f22
Gek <gakidogeck@gmail.com>
2023-06-12 12:04:20 -0700

    edit playlist works?

--- react-app/src/components/EditPlaylist/index.js
+++ react-app/src/components/EditPlaylist/index.js
@@ -1,4 +1,4 @@
-import { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import * as PlaylistActions from "../../store/playlists";
@@ -9,43 +9,64 @@ const EditPlaylist = () => {
     const { playlistId } = useParams();
     const history = useHistory();
 
-    const playlists = useSelector((state) => (state.playlists.playlists));
-    console.log('this is playlist',playlists)
-    // const playlist = playlists.find((playlist) => playlist.id === playlistId);
+    const currentPlaylist = useSelector(
+        (state) => state.playlists.playlists[playlistId]
+    );
     const user = useSelector((state) => state.session.user);
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
     const [errors, setErrors] = useState({});
 
-    const currentPlaylist = useSelector((state) => state.playlists.currentPlaylist);
-
     useEffect(() => {
-        dispatch(PlaylistActions.getPlaylistThunk(playlistId));
-    }, [dispatch, playlistId]);
-
-    // useEffect(() => {
-    //     if (playlist) {
-    //         setName(playlist.name);
-    //         setDescription(playlist.description);
-    //     }
-    // }, [playlist]);
+        if (currentPlaylist) {
+        setName(currentPlaylist.name);
+        setDescription(currentPlaylist.description);
+        }
+    }, [currentPlaylist]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const payload = {
-            name,
-            description,
+        id: playlistId,
+        name,
+        description,
         };
-        const data = await dispatch(PlaylistActions.updatePlaylistThunk(payload, playlistId));
-        if (data){
-            history.push('/playlists/current');
+
+        const data = await dispatch(
+        PlaylistActions.updatePlaylistThunk(user.id, payload)
+        );
+        if (data) {
+        history.push(`/users/${user.id}/playlists/${playlistId}`);
         }
-    }
+    };
 
-    return(
-        <>edit playlist</>
-    )
-}
+    return (
+        <form className="edit-playlist-form" onSubmit={handleSubmit}>
+        <h2>Edit Playlist</h2>
+        <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+        </div>
+        <div className="form-group">
+            <button type="submit">Update</button>
+        </div>
+        </form>
+    );
+};
 
-export default EditPlaylist
+export default EditPlaylist;
