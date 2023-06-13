54d3d663a92814692ce88afcac8b00607c12de75
Gek <gakidogeck@gmail.com>
2023-06-12 22:48:44 -0700

    fixed the user empty obj in landing page

--- react-app/src/components/Song/index.js
+++ react-app/src/components/Song/index.js
@@ -26,7 +26,9 @@ function Song() {
 
   return (
     <div className="song-container">
+
       {songs.map((song) => {
+        if (!song.imgUrl) { return null }
         const imgUrl =
           song.imgUrl && song.imgUrl.length > 0 ? song.imgUrl[0].imgUrl : null;
         return (
