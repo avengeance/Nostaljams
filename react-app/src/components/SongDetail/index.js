5949d3f3b4faa85ae9da2ed51ad02c2ff7aee869
Calvin Le <email.calvinle@gmail.com>
2023-06-12 18:12:25 -0700

    like and unlike now working

--- react-app/src/components/SongDetail/index.js
+++ react-app/src/components/SongDetail/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useModal } from "../../context/Modal";
 
 import OpenModalButton from "../OpenModalButton";
@@ -11,30 +11,21 @@ import * as SongActions from "../../store/songs";
 import * as LikesActions from "../../store/likes";
 import * as CommentActions from "../../store/comments";
 
-// import "./SongDetail.css";
+import "./SongDetail.css";
 
 const SongDetail = () => {
     const { songId } = useParams();
-    const { setModalContent } = useModal();
     const { closeModal } = useModal();
 
     const currentSong = useSelector((state) => state.songs.songs[songId]);
     const userId = useSelector((state) => state.session.user?.id);
-    const comment = useSelector((state) => state.comments.comments);
-
-    const songLikesCount = currentSong?.SongLikesCnt;
-    const userHasLiked = currentSong?.SongLikes?.some((like) => like.userId === userId);
+    const likesBySong = useSelector((state) => state.likes.likesBySong);
 
     const [comments, setComments] = useState([]);
     const [refreshKey, setRefreshKey] = useState(0);
     const [liked, setLiked] = useState(false);
 
-    const [song, setSong] = useState(null);
-    // const [userHasLiked, setUserHasLiked] = useState(false);
-
-
     const dispatch = useDispatch();
-    const history = useHistory();
 
     useEffect(() => {
         if (!songId) {
@@ -43,10 +34,7 @@ const SongDetail = () => {
         }
 
         dispatch(SongActions.getSongThunk(songId))
-        // .then((currentSong) => {
-        //     setSong(currentSong)
-        // }
-        // );
+
     }, [dispatch, songId]);
 
 
@@ -56,49 +44,60 @@ const SongDetail = () => {
     }, [dispatch, songId, refreshKey]);
 
     const handleLike = () => {
-        // if (!liked) {
         dispatch(LikesActions.createSongLikeThunk(songId))
             .then(() => {
                 setLiked(true);
                 dispatch(SongActions.getSongThunk(songId));
                 setRefreshKey(refreshKey + 1);
             });
-        // }
     };
 
-    function handlePostComment() {
-        const modalContent = (
-            <CreateCommentModal
-                onCommentSubmit={handlePostComment}
-            />)
-        history.push(`/songs/${songId}`);
-        setModalContent(modalContent);
+    const handleUnlike = () => {
+        const likesObj = Object.values(likesBySong).find(
+            like => like.userId === userId && like.songId === parseInt(songId));
+        if (!likesObj) {
+            console.error('No like found for the current user and song')
+            return
+        }
+        const likeId = likesObj.id
+        dispatch(LikesActions.deleteLikeBySongThunk(songId, likeId))
+            .then(() => {
+                setLiked(false);
+                dispatch(SongActions.getSongThunk(songId));
+                setRefreshKey(refreshKey + 1);
+            })
     }
 
     return (
         <div className="song-detail">
             {currentSong && (
-                <div className="song-info">
-                    <img
-                        src={currentSong?.SongImage}
-                        className="song-image"
-                        alt={currentSong?.name || 'song-image'}
-                    ></img>
-                    <div className="song-actions">
-                        <p>{currentSong.SongLikesCnt}</p>
-                        {!liked && (
-                            <button onClick={handleLike}>
-                                Like
-                            </button>
-                        )}
-                        {/* <button onClick={handleUnlike}>
-                            Unlike
-                        </button> */}
+                <div >
+                    <div className="song-detail-container">
+                        <div className="song-info">
+                            <img
+                                src={currentSong?.SongImage}
+                                className="song-image"
+                                alt={currentSong?.name || 'song-image'}
+                            ></img>
+                            <div className="song-actions">
+                                <p>{currentSong.SongLikesCnt}</p>
+                                {!liked && (
+                                    <button onClick={handleLike}>
+                                        Like
+                                    </button>
+                                )}
+                                {liked && (<button onClick={handleUnlike}>
+                                    Unlike
+                                </button>)}
+                            </div>
+                            <div className="song-info-details">
+                                <h2>{currentSong.name}</h2>
+                                <p>Artist: {currentSong.artists}</p>
+                                <p>Genre: {currentSong.genre}</p>
+                                <p>Description: {currentSong.description}</p>
+                            </div>
+                        </div>
                     </div>
-                    <h2>{currentSong.name}</h2>
-                    <p>Artist: {currentSong.artists}</p>
-                    <p>Genre: {currentSong.genre}</p>
-                    <p>Description: {currentSong.description}</p>
                     <div className='comment-button'>
                         {userId && (
                             <OpenModalButton
