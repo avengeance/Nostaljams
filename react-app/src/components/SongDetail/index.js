afd47ab2e6bc8ad52cac0ec1305bb33e23300240
Calvin Le <email.calvinle@gmail.com>
2023-06-13 18:59:04 -0700

    push

--- react-app/src/components/SongDetail/index.js
+++ react-app/src/components/SongDetail/index.js
@@ -16,217 +16,156 @@ import * as CommentActions from "../../store/comments";
 import "./SongDetail.css";
 
 const SongDetail = () => {
-  const { songId } = useParams();
-  const { closeModal } = useModal();
+    const { songId } = useParams();
+    const { closeModal } = useModal();
 
-  const currentSong = useSelector((state) => state.songs.songs[songId]);
-  const userId = useSelector((state) => state.session.user?.id);
-  const likesBySong = useSelector((state) => state.likes.likesBySong);
+    const { curSong, setCurSong } = usePlayer();
 
-  const [comments, setComments] = useState([]);
-  const [refreshKey, setRefreshKey] = useState(0);
-  const [liked, setLiked] = useState(false);
+    const currentSong = useSelector((state) => state.songs.songs[songId]);
+    const userId = useSelector((state) => state.session.user?.id);
+    const likesBySong = useSelector((state) => state.likes.likesBySong);
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (!songId) {
-      console.error("No songId");
-      return;
+    const [comments, setComments] = useState([]);
+    const [refreshKey, setRefreshKey] = useState(0);
+    const [liked, setLiked] = useState(false);
+
+        const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!songId) {
+        console.error("No songId");
+        return;
     }
 
-    dispatch(SongActions.getSongThunk(songId));
+        dispatch(SongActions.getSongThunk(songId));
 
-    dispatch(LikesActions.getLikesBySongThunk(songId))
-      .then(likes => {
-        const userLike = likes.find(
-          like => like.userId === userId
+        dispatch(LikesActions.getLikesBySongThunk(songId))
+            .then(likes => {
+                const userLike = likes.find(
+                    like => like.userId === userId
+                );
+
+                setLiked(!!userLike);  // !! turns the value into a boolean
+            });
+
+    }, [dispatch, songId, userId]);
+
+    useEffect(() => {
+        dispatch(CommentActions.getAllCommentsBySongThunk(songId)).then(
+        (comments) => setComments(comments)
         );
+    }, [dispatch, songId, refreshKey]);
 
-        setLiked(!!userLike);
-      });
-
-  }, [dispatch, songId, userId]);
-  // useEffect(() => {
-  //     if (!songId) {
-  //         console.error("No songId");
-  //         return;
-  //     }
-
-  //     dispatch(SongActions.getSongThunk(songId))
-
-  // }, [dispatch, songId]);
-
-
-  useEffect(() => {
-    dispatch(CommentActions.getAllCommentsBySongThunk(songId))
-      .then((comments) => setComments(comments));
-  }, [dispatch, songId, refreshKey]);
-
-  const handleLike = () => {
-    dispatch(LikesActions.createSongLikeThunk(songId))
-      .then(() => {
+    const handleLike = () => {
+        dispatch(LikesActions.createSongLikeThunk(songId)).then(() => {
         setLiked(true);
         dispatch(SongActions.getSongThunk(songId));
         setRefreshKey(refreshKey + 1);
-      });
-  };
+        });
+    };
 
-  const handleUnlike = () => {
-    const likesObj = Object.values(likesBySong).find(
-      like => like.userId === userId && like.songId === parseInt(songId));
-    if (!likesObj) {
-      console.error('No like found for the current user and song')
-      return
-    }
-    const likeId = likesObj.id
-    dispatch(LikesActions.deleteLikeBySongThunk(songId, likeId))
-      .then(() => {
+    const handleUnlike = () => {
+        const likesObj = Object.values(likesBySong).find(
+        (like) => like.userId === userId && like.songId === parseInt(songId)
+        );
+        if (!likesObj) {
+        console.error("No like found for the current user and song");
+        return;
+        }
+        const likeId = likesObj.id;
+        dispatch(LikesActions.deleteLikeBySongThunk(songId, likeId)).then(() => {
         setLiked(false);
         dispatch(SongActions.getSongThunk(songId));
         setRefreshKey(refreshKey + 1);
-      })
-
-    //   const { songId } = useParams();
-    //   const { closeModal } = useModal();
-
-    //   const { curSong, setCurSong } = usePlayer();
-
-    //   const currentSong = useSelector((state) => state.songs.songs[songId]);
-    //   const userId = useSelector((state) => state.session.user?.id);
-    //   const likesBySong = useSelector((state) => state.likes.likesBySong);
-
-    //   const [comments, setComments] = useState([]);
-    //   const [refreshKey, setRefreshKey] = useState(0);
-    //   const [liked, setLiked] = useState(false);
-
-    //   const dispatch = useDispatch();
-
-    //   useEffect(() => {
-    //     if (!songId) {
-    //       console.error("No songId");
-    //       return;
-
-    //     }
-
-    //     dispatch(SongActions.getSongThunk(songId));
-    //     console.log(currentSong);
-    //   }, [dispatch, songId]);
-
-    //   useEffect(() => {
-    //     dispatch(CommentActions.getAllCommentsBySongThunk(songId)).then(
-    //       (comments) => setComments(comments)
-    //     );
-    //   }, [dispatch, songId, refreshKey]);
-
-    //   const handleLike = () => {
-    //     dispatch(LikesActions.createSongLikeThunk(songId)).then(() => {
-    //       setLiked(true);
-    //       dispatch(SongActions.getSongThunk(songId));
-    //       setRefreshKey(refreshKey + 1);
-    //     });
-    //   };
-
-    //   const handleUnlike = () => {
-    //     const likesObj = Object.values(likesBySong).find(
-    //       (like) => like.userId === userId && like.songId === parseInt(songId)
-    //     );
-    //     if (!likesObj) {
-    //       console.error("No like found for the current user and song");
-    //       return;
-    //     }
-    //     const likeId = likesObj.id;
-    //     dispatch(LikesActions.deleteLikeBySongThunk(songId, likeId)).then(() => {
-    //       setLiked(false);
-    //       dispatch(SongActions.getSongThunk(songId));
-    //       setRefreshKey(refreshKey + 1);
-    //     });
-    //   };
+        });
+    };
 
     return (
-      <div className="song-detail">
+        <div className="song-detail">
         {currentSong && (
-          <div>
+            <div>
             <div className="song-detail-container">
-              <div className="song-info">
+                <div className="song-info">
                 <button
-                  className="audioPlayer__button_album_art"
-                  onClick={() => {
+                    className="audioPlayer__button_album_art"
+                    onClick={() => {
                     setCurSong(currentSong.audio_url);
-                  }}
+                    }}
                 >
-                  <img
+                    <img
                     src={currentSong?.SongImage}
                     className="song-image"
                     alt={currentSong?.name || "song-image"}
-                  ></img>
+                    ></img>
                 </button>
                 <div className="song-actions">
-                  <p id="song-likes">{currentSong.SongLikesCnt}</p>
-                  {!liked && (
+                    <p id="song-likes">{currentSong.SongLikesCnt}</p>
+                    {!liked && (
                     <button onClick={handleLike}>
-                      <i class="fas fa-thumbs-up"></i>
+                        <i class="fas fa-thumbs-up"></i>
                     </button>
-                  )}
-                  {liked && (
+                    )}
+                    {liked && (
                     <button onClick={handleUnlike}>
-                      <i class="fas fa-thumbs-down"></i>
+                        <i class="fas fa-thumbs-down"></i>
                     </button>
-                  )}
+                    )}
                 </div>
                 <div className="song-info-details">
-                  <h2>{currentSong.name}</h2>
-                  <span>Artist: </span>
-                  <p className="song-description">{currentSong.artists}</p>
-                  <span>Genre: </span>
-                  <p className="song-description">{currentSong.genre}</p>
-                  <span>Description: </span>
-                  <p className="song-description">{currentSong.description}</p>
+                    <h2>{currentSong.name}</h2>
+                    <span>Artist: </span>
+                    <p className="song-description">{currentSong.artists}</p>
+                    <span>Genre: </span>
+                    <p className="song-description">{currentSong.genre}</p>
+                    <span>Description: </span>
+                    <p className="song-description">{currentSong.description}</p>
                 </div>
-              </div>
+                </div>
             </div>
             <div className="comment-section">
-              <h2>Comments</h2>
-              <div className="comment-button">
+                <h2>Comments</h2>
+                <div className="comment-button">
                 {userId && (
-                  <OpenModalButton
+                    <OpenModalButton
                     buttonText={<i class="fas fa-comment">Add comment</i>}
                     modalComponent={
-                      <CreateCommentModal
+                        <CreateCommentModal
                         songId={songId}
                         onCommentSubmit={() => setRefreshKey(refreshKey + 1)}
                         refreshKey={refreshKey}
                         setRefreshKey={setRefreshKey}
-                      />
+                        />
                     }
-                  />
+                    />
                 )}
                 <div className="song-comments">
-                  {comments.map((comment, index) => (
+                    {comments.map((comment, index) => (
                     <div key={index}>
-                      <p>{comment.comment}</p>
-                      {userId === comment.userId && (
+                        <p>{comment.comment}</p>
+                        {userId === comment.userId && (
                         <OpenModalButton
-                          buttonText={<i class="fas fa-trash">Delete Comment</i>}
-                          modalComponent={
+                            className="delete-comment"
+                            buttonText={<i class="fas fa-trash">Delete Comment</i>}
+                            modalComponent={
                             <DeleteComment
-                              songId={songId}
-                              commentId={comment.id}
-                              closeModal={closeModal}
-                              refreshKey={refreshKey}
-                              setRefreshKey={setRefreshKey}
+                                songId={songId}
+                                commentId={comment.id}
+                                closeModal={closeModal}
+                                refreshKey={refreshKey}
+                                setRefreshKey={setRefreshKey}
                             />
-                          }
+                            }
                         />
-                      )}
+                        )}
                     </div>
-                  ))}
+                    ))}
                 </div>
-              </div>
+                </div>
             </div>
-          </div>
+            </div>
         )}
-      </div>
+        </div>
     );
-  };
-}
+};
+
 export default SongDetail;
